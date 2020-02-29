a08be845f5a6177b4e40eb2264586cc1aaabaf36
abuabayda <58151959+abuabayda@users.noreply.github.com>
2020-02-29 15:51:33 -0500

    Update CancelReservation.java

--- src/Test/CancelReservation.java
+++ src/Test/CancelReservation.java
@@ -3,5 +3,6 @@ package Test;
 public class CancelReservation {
     public static void main(String[] args) {
         System.out.println("Cancel Reservation room!");
+        System.out.println("Second");
     }
 }
